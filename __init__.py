97d04d59020edf7aa68c6bc7904a66d93ae83156
AbCooly <2553997408@qq.com>
2022-10-01 23:30:24 +0800

    可添加多个配置

--- __init__.py
+++ __init__.py
@@ -1,38 +1,95 @@
 from nonebot.adapters.onebot.v11 import MessageEvent, Bot
-from nonebot.rule import Rule
 from nonebot.plugin import on_message
-from .config import config
+from .config import contain, Config
+import re
+
+configs = contain.get_contain()
+
+
+# 规则检查
+async def rule_checker(event: MessageEvent) -> bool:
+    res = False
+    for cf in configs:
+        if group_checker(event, cf) and type_checker(event, cf) and qq_checker(event, cf):
+            cf.state_true()
+            res = True
+    return res
 
 
 # 检查类型
-async def type_checker(event: MessageEvent) -> bool:
-    if not config.type:     # 不填返回true
-        return True
+def type_checker(event: MessageEvent, cf: Config) -> bool:
     m = str(event.get_message())
-    for t in config.type:
-        if t in m:
-            return True
-    return False
+    p = re.compile(r"CQ:(.*),")
+    t = p.search(m)
+    if t:
+        t = t.group(1)
+        cf.set_mg_type(t)   # 设置消息类型
+    if not cf.ty:  # 不填返回true
+        return True
+    else:
+        return t in cf.ty
 
 
 # 检查群组
-async def group_checker(event: MessageEvent) -> bool:
-    if not config.obj:   # 未填返回true
+def group_checker(event: MessageEvent, cf: Config) -> bool:
+    if not cf.obj:  # 未填返回true
         return True
+    if event.message_type != "group":
+        return False
     g = event.get_session_id().split('_')[1]
-    return g in config.obj
+    return g in cf.obj
 
 
 # 检查具体对象
-async def qq_checker(event: MessageEvent) -> bool:
-    if not config.qq:   # 未填返回true
+def qq_checker(event: MessageEvent, cf: Config) -> bool:
+    if not cf.qq:  # 未填返回true
         return True
     q = event.get_user_id()
-    return q in config.qq
+    return q in cf.qq
 
 
-listen = on_message(rule=Rule(type_checker, group_checker, qq_checker))
+listen = on_message(rule=rule_checker)
+
+
 @listen.handle()
 async def _(bot: Bot, event: MessageEvent):
-    for t in config.sd:
-        await bot.send_group_msg(group_id=t, message=event.get_message())
+    for cf in configs:
+        # 判断这个config状态
+        if cf.get_state():
+            cf.state_false()
+            for t in cf.sd:
+                mg = event.get_message()
+                if cf.get_mg_type() not in ["redbag"]:
+                    if cf.get_mg_type() in ["record", "forward", "json"]:   # 转发不了,直接发送
+                        await bot.send_group_msg(group_id=t, message=mg)
+                    else:  # 转发消息
+                        await bot.call_api("send_group_forward_msg", **{
+                            "group_id": t,
+                            "messages": {
+                                "type": "node",
+                                "data": {
+                                    "id": event.message_id,
+                                }
+                            }})
+
+                # 直接发送
+                # await bot.send_group_msg(group_id=t, message=mg)
+
+                # 发送消息节点
+                # qq = event.get_user_id()
+                # if cf.get_mg_type() in ["record", "forward", "json"]:
+                #     await bot.send_group_msg(group_id=t, message=mg)
+                # else:
+                #     info = await bot.call_api("get_stranger_info", **{
+                #         "user_id": qq
+                #     })
+                #     await bot.call_api("send_group_forward_msg", **{
+                #         "group_id": t,
+                #         "messages": {
+                #             "type": "node",
+                #             "data": {
+                #                 "name": info["nickname"],
+                #                 "uin": qq,
+                #                 "content": mg
+                #             }
+                #         }})
--- config.py
+++ config.py
@@ -1,14 +1,51 @@
 # 配置文件
 
 class Config:
-    # 监视类型  不填-所有类型 forward-聊天记录 image-图片 record-语音 具体参考cq码 https://docs.go-cqhttp.org/cqcode/
-    type = ["forward", "video"]
-    # 监视群(不填-所有
-    obj = ["427956626", "1234567"]
-    # 具体对象(不填-所有
-    qq = []
-    # 发送群
-    sd = ["1234567"]
+    def __init__(self, ty=None, obj=None, qq=None, sd=None):
+        self.ty = ty
+        self.obj = obj
+        self.qq = qq
+        self.sd = sd
+        self.state = False
+        self.mg_type = "None"
+
+    def state_true(self):
+        self.state = True
+
+    def state_false(self):
+        self.state = False
+
+    def get_state(self):
+        return self.state
+
+    def set_mg_type(self, mg_type: str):
+        self.mg_type = mg_type
+
+    def get_mg_type(self):
+        return self.mg_type
 
 
-config = Config()
+class Contain:
+    def __init__(self, *args: Config):
+        # 存储Config
+        self.contain = []
+        for cf in args:
+            self.contain.append(cf)
+
+    def __call__(self, c: Config):
+        self.contain.append(c)
+
+    def get_contain(self):
+        return self.contain
+
+
+# 填写配置-例子
+# Config
+    # ty 监视类型  不填-所有类型 forward-聊天记录 image-图片 record-语音 具体参考cq码 https://docs.go-cqhttp.org/cqcode/
+    # obj 监视群(不填-所有
+    # qq 具体对象(不填-所有
+    # sd 发送群
+config = Config(["forward", "video"], ["123456", "321321"], [], ["427956626"])
+config2 = Config([], ["2376567356"], ["298587827"], ["427956626"])
+# 加入多个配置到容器中
+contain = Contain(config, config2)
